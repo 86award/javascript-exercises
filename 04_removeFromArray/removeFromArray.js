3e1032f5ecaad4d97ebfb45b6b53d65eed668677
Andy <58709759+86award@users.noreply.github.com>
2025-03-04 21:36:23 +0000

    ex04 complete. not an elegant solution.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...args) {
+    // get two arrays initialised
+    args.forEach(element => {
+        for (let i = 0; i < originalArray.length; i++) {
+            if (originalArray[i] === element)
+            {
+                originalArray.splice(i, 1);
+                i = 0;
+            }
+        }
+    });
+    return originalArray;
 };
 
 // Do not edit below this line
