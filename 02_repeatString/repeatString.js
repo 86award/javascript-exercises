38afb0053fd253fc2b16d3fcba153f07443e9755
Andy <58709759+86award@users.noreply.github.com>
2025-03-04 17:01:01 +0000

    ex2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringToReturn = '';
+    if (num < 0)
+    {
+        return stringToReturn = "ERROR";
+    }
+    for (let index = 0; index < num; index++) {
+        stringToReturn += string;
+    }
+    return stringToReturn;
 };
 
 // Do not edit below this line
