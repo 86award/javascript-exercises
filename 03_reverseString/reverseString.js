9d7abacffedf0a23e8376d492f9f9145cfb9a27a
Andy <58709759+86award@users.noreply.github.com>
2025-03-04 17:12:25 +0000

    ex03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToBeReversed)
+{
+    let reversedStringArray = Array.from(stringToBeReversed);
+    reversedStringArray.reverse();
+    reversedString = reversedStringArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
